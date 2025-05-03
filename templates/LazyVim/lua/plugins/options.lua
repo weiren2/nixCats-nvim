3e52bc4ffc03b1b55aca258537be83e8db36d0e6
Wei Ren <6765243+weiren2@users.noreply.github.com>
2025-05-03 17:28:00 -0500

    Update sync workflow and add file for custom vim options

---
+++ templates/LazyVim/lua/plugins/options.lua
@@ -0,0 +1,7 @@
+-- Vim/Nvim Customizations:
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+opt.autowrite = false      -- Auto write
+opt.relativenumber = false -- Relative line numbers
