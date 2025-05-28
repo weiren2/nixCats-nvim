bed511e47907a689152edfe82ef53848b1ec4e8e
Wei Ren <6765243+weiren2@users.noreply.github.com>
2025-05-28 10:15:59 -0500

    Update autocmds.lua

--- templates/LazyVim/lua/config/autocmds.lua
+++ templates/LazyVim/lua/config/autocmds.lua
@@ -7,32 +7,11 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-
--- Create an autocommand group to hold the cursor shape autocommands.
--- { clear = true } ensures that the group is cleared when this code is re-sourced,
--- preventing duplicate autocommands.
-local cursorShapeGroup = vim.api.nvim_create_augroup("CursorShapeAutocmds", { clear = true })
-
--- Define the guicursor strings
-local guicursor_active = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-local guicursor_inactive = "a:block-blinkon0"
-
--- Autocommand for when Neovim is entered or resumed
-vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-    group = cursorShapeGroup,
-    pattern = "*",
-    desc = "Set active cursor shape on VimEnter/VimResume",
-    callback = function()
-        vim.opt.guicursor = guicursor_active
-    end,
-})
-
 -- Autocommand for when Neovim is left or suspended
 vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-    group = cursorShapeGroup,
     pattern = "*",
-    desc = "Set inactive/block cursor shape on VimLeave/VimSuspend",
+    desc = "Set cursor shape to vertical bar on VimLeave/VimSuspend",
     callback = function()
-        vim.opt.guicursor = guicursor_inactive
+        vim.opt.guicursor = 'a:ver25'
     end,
 })
